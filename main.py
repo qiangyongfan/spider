e0c0f8a441326724b6eb37d84cd88f1fc518b6ad
范永强 <fanyongqiang765@163.com>
2021-10-27 01:34:42 +0800

    新增处理动态网页的函数,获取小区信息

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from lxml import etree
 import requests
 import re
+import json
 # 设置展示的最大宽度
 desired_width = 3200
 pd.set_option('display.width', desired_width)
@@ -13,120 +14,143 @@ pd.set_option('colheader_justify', 'left')
 import warnings
 warnings.filterwarnings('ignore')
 
-def get_data(addr_name):
+def get_data(url):
     """获取指定url的数据, 并将结果新增city字段
        url:数据地址
        city:改地址的城市名称
     """
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 \
-    (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}
-    url = f'https://sh.lianjia.com/ershoufang/rs{addr_name}/'
     res = requests.get(url, headers=headers)
-    print(res.text)
+    # print(res.text)
     res_elements = etree.HTML(res.text)
     return res_elements
 
-
-def get_house_num(res_elements):
-    """获取小区在售的二手房套数"""
-    table = res_elements.xpath('//h2[@class="total fl"]/span')
+def get_resblock_id(res_elements):
+    """获取小区的resblock_id"""
+    # 提取目标数据的标签
+    table = res_elements.xpath('//div[@class="list-more"]/dl/dd/a[@rel="nofollow"]')
+    # 转换成字符串
     ss = etree.tostring(table[0], encoding='utf-8').decode()
-    num =re.findall('<span>(.+?)</span>.*',ss)[0].strip()
-    return num
+    # 正则提取resblock_id
+    resblock_id = re.findall('.+c(\d+)rs.+', ss)[0].strip()
+    return resblock_id
+
+
 
 
 def get_commu_info(res_elements):
     """获取小区相关的情况"""
+    # 获取小区的resblock_id
+    resblock_id = get_resblock_id(res_elements)
+    # 获取小区的信息
+    url = f'https://sh.ke.com/api/listtop?type=resblock&resblock_id={resblock_id}&source=ershou_xiaoqu'
+    res = pd.Series()
+    response = requests.get(url, headers=headers)
+    html = response.text
 
-    # 所属板块是动态的页面, 这种写法搞不定
-    region_name  = res_elements.xpath('//span[@class="agentCardResblockSubTitle"]/text()')
-    print(region_name)
+    # 将json解析为dict
+    html_dic = json.loads(html)
+    # 解析json
+    xiaoqu_info = html_dic['data']['info']
+    res['小区名称'] = xiaoqu_info['name'].strip()
+    res['区名'] = xiaoqu_info['districtName'].strip()
+    res['板块名称'] = xiaoqu_info['bizcircleName'].strip()
+    res['小区均价']= xiaoqu_info['unitPrice']
+    res['近90天成交套数'] = xiaoqu_info['90saleCount']
+    res['近30天带看数'] = xiaoqu_info['day30See']
+    res['正在出售套数'] = xiaoqu_info['sellNum']
+    return res
 
 
+def get_house_info(res_elements,commu_info):
+    """当小区有在售二手房时, 获取在售的房子信息, 包括所属板块,几室几厅,面积, 售价"""
 
-
-
-def get_house_info(res_elements):
-    """获取在售的房子信息, 包括所属板块,几室几厅,面积, 售价"""
-
-    def create_ser(columns,num):
-        house_res = pd.Series(index=columns)
-        house_res['在售套数'] = num
-        return house_res
     # 存储结果用的DataFrame
-    columns = ['在售套数', 'title', '板块名称', '小区名称', '室厅', '面积', '卧室朝向', '装修', '楼层', '年份', '类型', '总价', '单价']
-    res = pd.DataFrame(columns=columns)
-
+    df_house = pd.DataFrame(columns=columns)
     # 获取该小区在售的房屋套数
-    num = get_house_num(res_elements)
-    print(f'{addr_name} 共有{num}套二手房在售')
 
     # 获取在售房屋列表
     table = res_elements.xpath('//div[@class="info clear"]')
     print('-' * 60)
+    # 提取在售房屋的信息
+    for i,house in enumerate(table):
+        house_res = commu_info.copy()
+        print(f'第{i+1}套')
+        # 将选出来得条目转成text, 再转成网页树
+        ss = etree.tostring(house,encoding='utf-8').decode()
+        house = etree.HTML(ss)
 
-    # 如果在售为0套, 新建一个空的Series, 包含在售的套数
-    if int(num)==0:
-        house_res = create_ser(columns, num)
-        res = res.append(house_res,ignore_index=True)
-    else:
-        # 提取在售房屋的信息
-        for i,houss in enumerate(table):
-            house_res = create_ser(columns, num)
-            print(f'第{i+1}套')
-            # 将选出来得条目转成text, 再转成网页树
-            ss = etree.tostring(houss,encoding='utf-8').decode()
-            houss = etree.HTML(ss)
+        # 提取房子信息
+        # 标题
+        title = house.xpath('//div[@class="title"]/a[@class="LOGCLICKDATA "]/text()')[0]
+        print(f"title:{title}")
+        house_res['title'] = title
 
-            # 提取房子信息
-            # 标题
-            title = houss.xpath('//div[@class="title"]/a[@class="LOGCLICKDATA "]/text()')[0]
-            print(f"title:{title}")
-            house_res['title'] = title
+        # 板块
+        region_name = house.xpath('//div[@class="positionInfo"]/a/text()')
+        print(f"板块名称:{region_name}")
+        if house_res['小区名称']!=region_name[0].strip():
+            continue
+        # 房屋信息
+        house_info = house.xpath('//div[@class="houseInfo"]/text()')[0]
+        print(f"房屋信息:{house_info}")
+        house_res['室厅'] = house_info.split('|')[0]
+        house_res['面积'] = house_info.split('|')[1]
+        house_res['卧室朝向'] = house_info.split('|')[2]
+        house_res['装修'] = house_info.split('|')[3]
+        house_res['楼层'] = house_info.split('|')[4]
+        house_res['年份'] = house_info.split('|')[5]
+        house_res['类型'] = house_info.split('|')[6]
 
-            # 板块
-            region_name = houss.xpath('//div[@class="positionInfo"]/a/text()')
-            print(f"板块名称:{region_name}")
-            house_res['板块名称'] = region_name[1]
-            house_res['小区名称'] = region_name[0]
+        # 价格
+        price = house.xpath('//div[@class="priceInfo"]/div[@class="totalPrice totalPrice2"]')[0].xpath('string(.)')
+        print(f'总价:{price}')
+        house_res['总价'] = price
 
-            # 房屋信息
-            house_info = houss.xpath('//div[@class="houseInfo"]/text()')[0]
-            print(f"房屋信息:{house_info}")
-            house_res['室厅'] = house_info.split('|')[0]
-            house_res['面积'] = house_info.split('|')[1]
-            house_res['卧室朝向'] = house_info.split('|')[2]
-            house_res['装修'] = house_info.split('|')[3]
-            house_res['楼层'] = house_info.split('|')[4]
-            house_res['年份'] = house_info.split('|')[5]
-            house_res['类型'] = house_info.split('|')[6]
+        # 单价
+        unit_price = house.xpath('//div[@class="priceInfo"]/div[@class="unitPrice"]')[0].xpath('string(.)')
+        print(f"单价:{unit_price}")
+        house_res['单价'] = unit_price
 
-            # 价格
-            price = houss.xpath('//div[@class="priceInfo"]/div[@class="totalPrice totalPrice2"]')[0].xpath('string(.)')
-            print(f'总价:{price}')
-            house_res['总价'] = price
-
-            # 单价
-            unit_price = houss.xpath('//div[@class="priceInfo"]/div[@class="unitPrice"]')[0].xpath('string(.)')
-            print(f"单价:{unit_price}")
-            house_res['单价'] = unit_price
-
-            print('-'*60)
-            res = res.append(house_res,ignore_index=True)
-    return res
+        print('-'*60)
+        df_house = df_house.append(house_res,ignore_index=True)
+    return df_house
 
 
-# addr_names = ['金领国际','龚路新村','龚华公寓','龚路新城']
-addr_names = ['龚路新村']
-res = []
-for addr_name in addr_names :
-    # 获取网页
-    res_elements = get_data(addr_name)
-    get_commu_info(res_elements)
 
-    # 获取二手房的信息
-#     res_df = get_house_info(res_elements)
-#     res.append(res_df)
-# result = pd.concat(res,axis=0)
-# print(result)
+def main(df):
+    """"""
+    res = []
+    for addr_name in addr_names:
+        print(f"{addr_name=}")
+        # 获取网页
+        url = f'https://sh.lianjia.com/ershoufang/rs{addr_name}/'
+        res_elements = get_data(url)
+        # 提取消息概况信息
+        commu_info = get_commu_info(res_elements)
+
+        # 获取二手房信息
+        if commu_info['正在出售套数'] ==0:
+            print(f'小区{addr_name}没有在售二手房')
+            df = df.append(commu_info,ignore_index=True)
+        else:
+            df_house = get_house_info(res_elements, commu_info)
+            df = pd.concat([df,df_house],ignore_index=True)
+
+    return df
+
+
+if __name__=="__main__":
+
+    addr_names = ['金领国际','龚路新村','龚华公寓','龚路新城','金利公寓']
+    # addr_names = ['金领国际','金利公寓']
+
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 \
+    (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}
+    columns = ['区名','板块名称','小区名称','小区均价', '正在出售套数','近30天带看数','近90天成交套数','title', '室厅', '面积', '卧室朝向', '装修', '楼层', '年份', '类型', '总价', '单价']
+    df = pd.DataFrame(columns=columns)
+
+    df = main(df)
+    print(df)
+
 
